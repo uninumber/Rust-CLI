7bf89735823022c4ed570eebec2a504168d49eaa
uninumber <futucp@gmail.com>
2023-02-23 21:38:01 +0300

    fix: [path]. add tests. add comments.

--- uniq/src/uniq.rs
+++ uniq/src/uniq.rs
@@ -1,8 +1,9 @@
 use anyhow::Context;
 use clap::{Arg, Command};
 use std::error::Error;
-use std::fs::File;
+use std::fs::{File, self};
 use std::io::{self, BufRead, BufReader, Write};
+use std::path::{Path, PathBuf};
 
 pub struct Config {
     pub in_file: String,
@@ -34,13 +35,14 @@ pub fn get_args() -> MyResult<Config> {
     let in_file = matches
         .get_one::<String>("in_file")
         .context("missing your file")?;
-    let file_output = matches
-        .get_one::<String>("file_output")
-        .context("cannot produce your file");
-    // let file_output = matches.get_one::<String>("file_output").map(|v| v.to_string());
+    // let file_output = matches
+    //     .get_one::<String>("file_output")
+    //     .context("cannot produce your file");
+    let file_output = matches.get_one::<String>("file_output").map(|v| v.to_string());
     Ok(Config {
         in_file: in_file.to_string(),
-        file_output: Some(file_output?.to_string()),
+        file_output,
+        // file_output: Some(file_output?.to_string()),
         count: matches.args_present(),
     })
 }
@@ -52,6 +54,10 @@ pub fn run(config: Config) -> MyResult<()> {
         Some(name_output) => Box::new(File::create(name_output)?),
         _ => Box::new(io::stdout()),
     };
+
+    let file_len = fs::read_to_string(&config.in_file)?
+        .lines()
+        .count();
 
     let mut print = |count: u64, file: &str| -> MyResult<()> {
         if count > 0 {
@@ -67,13 +73,13 @@ pub fn run(config: Config) -> MyResult<()> {
     let mut line = String::new();
     let mut previous = String::new();
     let mut count: u64 = 0;
-    loop {
-        let bytes = file.read_line(&mut line)?;
-        if bytes == 0 {
-            break;
-        }
 
-        if line.trim_end() != previous.trim_end() {
+    for _ in 0..=file_len {
+        let bytes = file.read_line(&mut line)?;
+        if bytes.eq(&0) {
+            eprintln!("Warning: file {} is empty", file);
+        }
+        if line != previous {
             print(count, &previous)?;
             previous = line.clone();
             count = 0;
@@ -81,8 +87,7 @@ pub fn run(config: Config) -> MyResult<()> {
         count += 1;
         line.clear()
     }
-    print(count, &previous)?;
-    Ok(())
+    print(count, &previous)
 }
 
 pub fn open(file: &str) -> MyResult<Box<dyn BufRead>> {

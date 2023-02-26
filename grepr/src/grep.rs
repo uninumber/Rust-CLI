707f40efb559ada1787bf8e2ab77186d4a21bb73
uninumber <futucp@gmail.com>
2023-02-26 20:43:45 +0300

    add tests, add functions, fix: recursive[args_present]

--- grepr/src/grep.rs
+++ grepr/src/grep.rs
@@ -1,104 +1,169 @@
-use anyhow::{Context};
-use std::fs::{self, File};
-use std::io::{BufReader, BufRead, Write, self};
+use anyhow::Context;
+use clap::{Arg, Command};
+use regex::{Regex, RegexBuilder};
 use std::error::Error;
-use clap::{Command, Arg};
+use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader};
+use std::mem;
+use walkdir::WalkDir;
+#[derive(Debug, Clone)]
 pub struct Config {
-    file_input: String,
-    file_output: Option<String>,
-    count: bool,
-    word: String
+    pub files: Vec<String>,
+    pub count: bool,
+    pub pattern: Regex,
+    // pub recursive: bool,
 }
 
 pub fn getting_args() -> MyResult<Config> {
     let arguments = Command::new("grepr")
         .author("c631c")
         .version("0.0.1")
-        .about("this is grep implementation written
-        written in Rust language with additional functionality.")
+        .about(
+            "this is grep implementation written
+        written in Rust language with additional functionality.",
+        )
         .term_width(100)
-        .arg(Arg::new("word")
-             .long("word")
-             .short('w')
-             .help("Provide the word you are searching for.")
-             )
-        .arg(Arg::new("file_input")
-             .long("file")
-             .short('f')
-             .required(true)
-             .ignore_case(true)
-             .help("Provide file(-s) just right 
-             after calling the command as the second argument.")
-             )
-        .arg(Arg::new("file_output")
-             .long("output")
-             .short('o')
-             )
-        .arg(Arg::new("count")
-             .long("count")
-             .short('c')
-             .help("Set as true if you want to see count of lines,
-             otherwise set as false")
-             )
+        .arg(
+            Arg::new("pattern")
+                .value_name("PATTERN")
+                .help("Search pattern")
+                .required(true),
+        )
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .num_args(1..)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::new("insensitive")
+                .short('i')
+                .long("insensitive")
+                .help("Case-insensitive"),
+        )
+        .arg(
+            Arg::new("recursive")
+                .short('r')
+                .long("recursive")
+                .help("Recursive search"),
+        )
+        .arg(
+            Arg::new("count")
+                .short('c')
+                .long("count")
+                .help("Count occurrences"),
+        )
+        .arg(
+            Arg::new("invert")
+                .short('v')
+                .long("invert-match")
+                .help("Invert match"),
+        )
         .get_matches();
 
-    let file_input = arguments.get_one::<String>("file_input").context("what")?;
-    let file_output = arguments.get_one::<String>("file_output");
-    let word = arguments.get_one::<String>("word").unwrap();
-    Ok( Config {
-        word: word.to_string(),
-        file_input: file_input.to_string(),
-        file_output: file_output.cloned().to_owned(),
-        count: arguments.args_present()
+    let files = arguments
+        .get_one::<String>("files")
+        .map(|v| vec![v.clone()])
+        .context("error occured during files initialization")?;
+    let pattern = arguments
+        .get_one::<String>("pattern")
+        .context("error ocurred during pattern initialization")?;
+    let pattern = RegexBuilder::new(&pattern)
+        .build()
+        .map_err(|_| format!("Invalid pattern \"{}\"", pattern))?;
+    Ok(Config {
+        pattern,
+        files,
+        count: arguments.args_present(),
+        // recursive: arguments.args_present(),
     })
-             
-             
 }
+
 type MyResult<T> = Result<T, Box<dyn Error>>;
+
 pub fn run(config: Config) -> MyResult<()> {
-    let mut file = open(&config.file_input)
-        .map_err(|err| format!("Got error: {err}"))?;
-    
-    let mut file_output: Box<dyn Write> = match config.file_output {
-        Some(file_output) => Box::new(File::create(file_output)?),
-        _ => Box::new(io::stdout()),
-    };
+    let entries = find_files(config.files);
+    let file_num = entries.len();
 
-    let mut print = | count: u64, text: &str | -> MyResult<()> {
-        if count > 0 {
-            if config.count {
-                write!(file_output, "{} : {}", count, text)?;
-            } else {
-                write!(file_output, "{}", text)?;
-            }
+    let print = |fname: &str, val: &str| {
+        if file_num > 0 {
+            print!("{} : {}", fname, val)
+        } else {
+            print!("{}", val);
         }
-        Ok(())
     };
 
-    let mut another_print = |text: &str | -> MyResult<()> {
-        write!(file_output, "{}", text)?;
-        Ok(())
-    };
+    for entry in entries {
+        match entry {
+            Err(e) => eprintln!("{}", e),
+            Ok(filename) => match open(&filename) {
+                Err(e) => eprintln!("{}: {}", filename, e),
+                Ok(file) => match find_lines(file, &config.pattern) {
+                    Err(e) => eprintln!("{}", e),
+                    Ok(files) => {
+                        for line in &files {
+                            print(&filename, line);
+                        }
+                    }
+                },
+            },
+        }
+    }
+    Ok(())
+}
 
+pub fn find_lines<T: BufRead>(
+    mut file: T,
+    pattern: &Regex) -> MyResult<Vec<String>> {
+    let mut matches = vec![];
     let mut line = String::new();
-    let mut previous = String::new();
-    let mut count: u64 = 0;
-    loop { 
-        let bytes = file.read_line(&mut line)?; if bytes == 0 {
+
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if line.contains(&pattern.to_string()) {
+            matches.push(mem::take(&mut line));
+        }
+        line.clear();
+        if bytes.eq(&0) {
             break;
         }
-
-        if line.contains(&config.word) {
-            print(count, &previous)?;
-            previous = line.clone();
-        }
-        count += 1;
-        line.clear();
     }
-    print(count, &previous)?;
-    Ok(())
-
+    Ok(matches)
 }
+
+fn find_files(paths: Vec<String>) -> Vec<MyResult<String>> {
+
+    let mut results = vec![];
+
+    for path in &paths {
+        match path.as_str() {
+            "-" => eprintln!("Provide some file"),
+            _ => match fs::metadata(&path) {
+                Ok(metadata) => {
+                    if metadata.is_dir() {
+                        // if recursive {
+                            for entry in WalkDir::new(path)
+                                .into_iter()
+                                .flatten()
+                                .filter(|v| v.file_type().is_file())
+                            {
+                                results.push(Ok(entry.path().display().to_string()));
+                            }
+                        // } else {
+                            results.push(Err(From::from(format!("{} is a directory", path))));
+                        // }
+                    } else if metadata.is_file() {
+                        results.push(Ok(path.to_string()));
+                    }
+                }
+                Err(e) => results.push(Err(From::from(format!("{}: {}", path, e)))),
+            },
+        }
+    }
+    results
+}
+
 pub fn open(file: &str) -> MyResult<Box<dyn BufRead>> {
     match file {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
